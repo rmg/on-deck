dbd57c643da8883b32c30d2697f74a9ef6b9305d
Ryan Graham <r.m.graham@gmail.com>
2012-11-08 01:03:59 -0800

    More minor tweaking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
              else
                'success'
              end
-    content_tag :span, class: "badge badge-#{colour}" do
+    content_tag :span, class: "badge badge-#{colour} h3" do
       "#{skill.name} #{present.count}/#{all.count}"
     end
   end
@@ -31,6 +31,8 @@ module ApplicationHelper
     content_tag :span, class: "badge badge-#{colour}" do
       if user.status == :break
         "Be back in #{distance_of_time_in_words(Time.now, user.away_until)}"
+      elsif user.status == :office
+        "Here for #{distance_of_time_in_words(Time.now, user.present_until)}"
       else
         user.status.to_s.titleize
       end
@@ -40,7 +42,7 @@ module ApplicationHelper
   def complex_button(object, label, data)
     form_for(object, html: {class: 'force-inline'}) do |f|
       fields = data.map { |col, val| f.hidden_field col, value: val }
-      fields << f.button(label, class: 'btn')
+      fields << f.button(label, class: 'btn btn-mini btn-primary')
       fields.join.html_safe
     end
   end
@@ -48,25 +50,29 @@ module ApplicationHelper
   def status_update_buttons(user)
     case user.status
     when :away
-      complex_button(user, "I'm here!",
-                     present_until: Time.now + 8.hours)
+      [ complex_button(user, "I'm here!",
+                       present_until: Time.now + 8.hours) ]
     when :office
-      complex_button(user, "I'm outta here!",
-                     present_until: Time.now) +
-      complex_button(user, "Be back in an hour!",
-                     away_until: Time.now + 1.hour) +
-      complex_button(user, "Be back in 15!",
-                     away_until: Time.now + 15.minutes)
+      [
+        complex_button(user, "I'm outta here!",
+                       present_until: 1.second.ago),
+        complex_button(user, "Be back in an hour!",
+                       away_until: Time.now + 1.hour),
+        complex_button(user, "Be back in 15!",
+                       away_until: Time.now + 15.minutes)
+      ]
     when :break
-      complex_button(user, "I'm back early!",
-                     away_until: Time.now) +
-      complex_button(user, "Gonna be another 30..",
-                     away_until: Time.now + 30.minutes) +
-      complex_button(user, "I'm outta here!",
-                     present_until: Time.now)
+      [
+        complex_button(user, "I'm back early!",
+                       away_until: 1.second.ago),
+        complex_button(user, "Gonna be another 30..",
+                       away_until: user.away_until + 30.minutes),
+        complex_button(user, "I'm outta here!",
+                       present_until: 1.second.ago)
+      ]
     else
-      complex_button(user, "I'm here!",
-                     present_until: Time.now + 8.hours)
+      [ complex_button(user, "I'm here!",
+                       present_until: Time.now + 8.hours) ]
     end
   end
 
