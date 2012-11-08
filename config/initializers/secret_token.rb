e20b5215130e61ee9c68db666dcc8b918c8035dd
Ryan Graham <r.m.graham@gmail.com>
2012-11-07 21:44:32 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnDeck::Application.config.secret_token = '973d05778405a45c6b4eeb539f1a929364a1a0a18f56bbf2410685250504ba00ce818c1feb9d48f11b5f8c1d650fe3fc0b281c38cbf473fdcedf4a7f5fbe0631'
