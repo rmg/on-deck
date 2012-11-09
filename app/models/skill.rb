fa4936b47891b17851721edf33ea68c307514bb2
Ryan Graham <r.m.graham@gmail.com>
2012-11-08 20:10:29 -0800

    Hide skills that aren't represented in the current domain

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,8 @@
 class Skill < ActiveRecord::Base
   attr_accessible :name
   has_and_belongs_to_many :users
+
+  scope :in_domain, ->(domain) {
+    joins(:users).where(users: {domain: domain}).uniq
+  }
 end
